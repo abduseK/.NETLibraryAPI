c977367ddf24374871f1ea4a4b6a6c41669e5741
abdelselam kemal <abdelselamkemal74@gmail.com>
2023-02-25 14:05:52 -0800

    init commit

---
+++ LibraryMgt.cs
@@ -0,0 +1,9 @@
+// namespace libraryManagement;
+
+//     public class LibraryMgt {
+//         public int BookId {get; get;}
+//         public string BookTitle {get; get;} = string.Empty;
+//         public string Author {get; get;} = string.Empty;
+//         public int NumOfPage {get; get;} = string.Empty;
+
+//     }
